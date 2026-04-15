bf3b5b04a1aa530d19a20c1d26cbe9f4b5ae6aaa
dev <am@123.com>
2026-04-15 16:38:19 +0800

    fix

--- deploy-remote.bat
+++ deploy-remote.bat
@@ -1,3 +1,4 @@
 set GOOS=linux
 go build -o nanping-pt-carbon ./cmd/main.go
 C:\Windows\System32\OpenSSH\scp.exe .\nanping-pt-carbon root@121.41.129.227:/home/app/nanping-carbon/tmp
+del /f /q .\nanping-pt-carbon
--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
 set GOOS=linux
 go build -o nanping-pt-carbon ./cmd/main.go
 C:\Windows\System32\OpenSSH\scp.exe .\nanping-pt-carbon root@115.236.162.150:/home/yanping/app/nanping/yanping-carbon/tmp
+del /f /q .\nanping-pt-carbon
